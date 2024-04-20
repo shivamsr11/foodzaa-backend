6e73264ddeb9716688aeb09c98fe8d7fbdc436e6
shivamsr11 <rajmaneshivam10@gmail.com>
2024-04-20 20:35:30 +0530

    connected backend to WatchMe

--- server.js
+++ server.js
@@ -2,14 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+console.log("hello")
+
 app.use(cors({
     origin:"http://localhost:3000",
 }));
-app.use(express.json());
+app.use(express.json()); 
+
+app.get('/api/searchSuggestions', (req, res) => {
+  const {searchString} = req.query;
+  console.log(req.query);
+  const url = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=${searchString}`;
+
+  fetch(url, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(res)
+      res.json(data);
+    })
+    .catch(error => {
+      console.error(error);
+      res.status(500).send('An error occurred');
+    });
+});
 
 app.get('/api/restaurants', (req, res) => {
-    // const { lat, lng } = req.query;
-    // console.log(req.query);
+    //const { lat, lng } = req.query;
+    console.log(req.query);
     const url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=16.7049873&lng=74.24325270000001&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
   
     fetch(url, {
@@ -26,6 +56,7 @@ app.get('/api/restaurants', (req, res) => {
         return response.json();
       })
       .then(data => {
+        console.log(res)
         res.json(data);
       })
       .catch(error => {
